30fb8f391cff74c07b29d7b8b219675b55b2e9bb
Ramesh Adhikari <ourworld898@gmail.com>
2025-03-09 21:41:36 +0545

    Added Features

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
-const ramesh = "ramesh adhikari" 
-let age = 5 
+const ramesh = "ramesh adhikari"
+let age = 5
 age = 30
 age = 60
 
-if(age>50){
+if (age > 50) {
     //code here
     age -= 10
-}else{
+} else {
     age += 10
 }
 console.log(age);
-for(let i = 1; i<= 10;i+=2){
+for (let i = 1; i <= 10; i += 2) {
     console.log(i);
 }
 
 
 let testObject = {
-    name : "itsName",
-    surname : "itsSurname",
-    age : 25,
-    class : 22
+    name: "itsName",
+    surname: "itsSurname",
+    age: 25,
+    class: 22
 }
 console.log(testObject["name"])
 console.log(testObject.name)
 
-function square(x){
-    return x*x;
+function square(x) {
+    return x * x;
 }
 
 console.log(square(2))
 
+// new features added
+console.log("new features");
+
+// This is overpowered
