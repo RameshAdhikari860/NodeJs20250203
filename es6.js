a97b1bb8bffc45e08bcbe77a151557345457f493
Ramesh Adhikari <ourworld898@gmail.com>
2025-03-09 21:31:32 +0545

    First Commit

---
+++ es6.js
@@ -0,0 +1,32 @@
+// const user = {
+//     name : "rame",
+//     age : 22,
+//     class : 35
+// }
+// console.log(user.name,user.age);
+
+// const {name,age}  = user;
+// console.log(name,age);
+
+// const next = {
+//     employe :"ram",
+//     shyam : "wow"
+// }
+// const final = {...user,...next};
+// console.log(final)
+
+// Higher order array methods 
+ // 1. Map [x,x,x] => [y,y,y]
+ // 2. filter [x,x,y] => [x,x]
+ // 3. sort [4,2,5] =>[2,4,5]
+
+//  const numbers = [4,6,74,35,63]
+
+//  numbers.map((num)=>console.log(num+5));
+
+//  const filteredNum = numbers.filter((num)=>num>50);
+//  console.log(filteredNum)
+const name = "age";
+
+const tDetails = `Hello my name is ${name}`
+console.log(tDetails);
---
+++ main.js
@@ -0,0 +1,32 @@
+const ramesh = "ramesh adhikari" 
+let age = 5 
+age = 30
+age = 60
+
+if(age>50){
+    //code here
+    age -= 10
+}else{
+    age += 10
+}
+console.log(age);
+for(let i = 1; i<= 10;i+=2){
+    console.log(i);
+}
+
+
+let testObject = {
+    name : "itsName",
+    surname : "itsSurname",
+    age : 25,
+    class : 22
+}
+console.log(testObject["name"])
+console.log(testObject.name)
+
+function square(x){
+    return x*x;
+}
+
+console.log(square(2))
+
